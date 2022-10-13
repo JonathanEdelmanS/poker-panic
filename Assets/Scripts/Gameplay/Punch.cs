5a867f8fdf94e09da5c64b9dec2dda09078995cc
nmorale5 <nmorale5@mit.edu>
2022-10-13 16:26:49 -0400

    better punches

--- Assets/Scripts/Gameplay/Punch.cs
+++ Assets/Scripts/Gameplay/Punch.cs
@@ -7,29 +7,31 @@ public class Punch : MonoBehaviour
     public GameObject enemy;
     public PokerManager pokerManager;
     public Collider2D thisCollider;
+    public float radius;
     public float expiration;
     float lifetime;
 
     public void Activate()
     {
-        thisCollider.enabled = true;
-        lifetime = expiration;
+        Vector3 thisPos = transform.position;
+        Vector3 enemyPos = enemy.transform.position;
+        if ((thisPos - enemyPos).magnitude < radius) pokerManager.Punch(enemy);
     }
 
-    private void Update()
-    {
-        lifetime -= Time.deltaTime;
-        if (lifetime <= 0f)
-        {
-            thisCollider.enabled = false;
-        }
-    }
+    //private void Update()
+    //{
+    //    lifetime -= Time.deltaTime;
+    //    if (lifetime <= 0f)
+    //    {
+    //        thisCollider.enabled = false;
+    //    }
+    //}
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        GameObject player = other.gameObject;
-        if (player.name != "Player 1" && player.name != "Player 2") return;
-        Debug.Log(player.name + " is here");
-        if (player == enemy) pokerManager.Punch(player);
-    }
+    //void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    GameObject player = other.gameObject;
+    //    if (player.name != "Player 1" && player.name != "Player 2") return;
+    //    Debug.Log(player.name + " is here");
+    //    if (player == enemy) pokerManager.Punch(player);
+    //}
 }
